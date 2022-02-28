d314c678c91528e6ef60f3b321a7cd985d71026e
Ubuntu <ubuntu@ip-172-31-3-227.ec2.internal>
2022-02-28 03:59:47 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+       select(:rating).map(&:rating).uniq#collection of movie ratings
+    end
+
+    def self.with_ratings(ratings)
+        if ratings.nil? #when no rating is checked
+            Movie.all
+        else #filtering by rating passed as param
+            Movie.where(rating:ratings)
+        end
+    end
 end
+
